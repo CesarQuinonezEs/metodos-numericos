4285215a93b0ee433ba9dc2ea0d12af86f7d1bec
CesarQuinonezEs <quinonez.cesar@uabc.edu.mx>
2022-08-11 15:38:39 -0700

    Guss jordan

---
+++ prubas_de_python/gussJordar.py
@@ -0,0 +1,28 @@
+import numpy as np
+import sys
+
+n = int(input('Ingrese cualquier numero '))
+a = np.zeros((n, n+1))
+x = np.zeros(n)
+print("Ingresa argumentos de la matriz")
+for i in range(n):
+    for j in range(n+1):
+        a[i][j] = float(input('a['+str(i)+']['+ str(j)+']='))
+
+for i in range(n):
+    if a[i][i] == 0.0:
+        sys.exit('Indefinido')
+
+    for j in range(n):
+        if i != j:
+            ratio = a[j][i]/a[i][i]
+
+            for k in range(n+1):
+                a[j][k] = a[j][k] - ratio * a[i][k]
+
+for i in range(n):
+    x[i] = a[i][n]/a[i][i]
+
+print('\n la solucion es: ') 
+for i in range(n):
+    print('X%d = %0.2f' %(i,x[i]), end = '\t')
